da6ecb134c1b873882439b23b5805aec5bffc4e7
Ruslan Ibragimov <ruslan@ibragimov.by>
2017-12-13 19:09:25 +0300

    Publish help.

--- PUBLISH.md
+++ PUBLISH.md
@@ -3,5 +3,5 @@
 1. edit `gradle.properties`
 1. `git tag 0.0.6`
 1. `git push origin 0.0.6`
-1. `./publish.sh`
+1. edit `./publish.sh` and then `./publish.sh`
 1. Publish https://bintray.com/heapy/heap/yml-ical
--- publish.sh
+++ publish.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-export BINTRAY_USER="irus"
-export BINTRAY_API_KEY=""
+# Uncomment and fill.
+#export BINTRAY_USER=""
+#export BINTRAY_API_KEY=""
 
 ./gradlew clean javadocJar sourcesJar assemble bintrayUpload
